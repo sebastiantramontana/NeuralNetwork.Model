8ba321543c53be419ab8073d95d1d3e68862f833
Sebastian <sebastiantramontana@gmail.com>
2019-06-27 14:09:01 -0300

    Reaload all elements in InputLayer when changes are firing

--- NeuralNetwork.Model/Layers/InputLayer.cs
+++ NeuralNetwork.Model/Layers/InputLayer.cs
@@ -5,93 +5,102 @@ using System.Linq;
 
 namespace NeuralNetwork.Model.Layers
 {
-    public class InputLayer : LayerBase<Input>
-    {
-        private IDictionary<string, Element> _allElements;
+   public class InputLayer : LayerBase<Input>
+   {
+      private IDictionary<string, Element> _allElements;
 
-        public InputLayer(string id) : base(id)
-        {
-            
-        }
+      public InputLayer(string id) : base(id)
+      {
+      }
 
-        /// <summary>
-        /// Find an element by id throughout the entire model
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns>The found element or null if it is not exist</returns>
-        public Element Find(string id)
-        {
-            if (string.IsNullOrWhiteSpace(id))
-                return null;
+      /// <summary>
+      /// Find an element by id throughout the entire model
+      /// </summary>
+      /// <param name="id"></param>
+      /// <returns>The found element or null if it is not exist</returns>
+      public Element Find(string id)
+      {
+         if (string.IsNullOrWhiteSpace(id))
+            return null;
 
-            _allElements.TryGetValue(id, out Element element);
-            return element;
-        }
+         _allElements.TryGetValue(id, out Element element);
+         return element;
+      }
 
-        /// <summary>
-        /// Find an typed element by id throughout the entire model
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns>The found element or null if itsn't exist or type doesn't match</returns>
-        public TElement Find<TElement>(string id) where TElement : Element
-        {
-            return Find(id) as TElement;
-        }
+      /// <summary>
+      /// Find an typed element by id throughout the entire model
+      /// </summary>
+      /// <param name="id"></param>
+      /// <returns>The found element or null if itsn't exist or type doesn't match</returns>
+      public TElement Find<TElement>(string id) where TElement : Element
+      {
+         return Find(id) as TElement;
+      }
 
-        public int GetMaxNodeCountInLayer()
-        {
-            int max = 0;
+      public int GetMaxNodeCountInLayer()
+      {
+         int max = 0;
 
-            for (LayerBase layer = this; layer != null; layer = layer.Next)
-            {
-                var count = layer.GetAllNodes().Count();
+         for (LayerBase layer = this; layer != null; layer = layer.Next)
+         {
+            var count = layer.GetAllNodes().Count();
 
-                if (count > max)
-                    max = count;
-            }
+            if (count > max)
+               max = count;
+         }
 
-            return max;
-        }
+         return max;
+      }
 
-        public int CountLayers()
-        {
-            int count = 0;
+      public int CountLayers()
+      {
+         int count = 0;
 
-            for (LayerBase layer = this; layer != null; layer = layer.Next, ++count) ; //nice! :)
+         for (LayerBase layer = this; layer != null; layer = layer.Next, ++count) ; //nice! :)
 
-            return count;
-        }
+         return count;
+      }
 
-        public LayerBase SearchOutputLayer()
-        {
-            LayerBase outputlayer;
-            for (outputlayer = this; outputlayer.Next != null; outputlayer = outputlayer.Next) ;
+      public LayerBase SearchOutputLayer()
+      {
+         LayerBase outputlayer;
+         for (outputlayer = this; outputlayer.Next != null; outputlayer = outputlayer.Next) ;
 
-            return outputlayer;
-        }
+         return outputlayer;
+      }
 
-        public void Validate()
-        {
-            LayerBase outputlayer = SearchOutputLayer();
+      public void Validate()
+      {
+         LayerBase outputlayer = SearchOutputLayer();
 
-            if (outputlayer == this)
-            {
-                throw new MissingOutputException(this);
-            }
+         if (outputlayer == this)
+         {
+            throw new MissingOutputException(this);
+         }
 
-            ///Bias doesn´t make sense to be in the output layer
-            ///TODO: Fix Model, make an OutputLayer
-            if (outputlayer.Bias != null)
-            {
-                throw new InvalidOutputBiasException(outputlayer);
-            }
+         ///Bias doesn´t make sense to be in the output layer
+         ///TODO: Fix Model, make an OutputLayer
+         if (outputlayer.Bias != null)
+         {
+            throw new InvalidOutputBiasException(outputlayer);
+         }
 
-            //Don't reuse _allElements! 
-            //if filled, it will throw a DuplicatedIdException for each call
-            var acumulatedIds = new Dictionary<string, Element>();
-            ValidateId(acumulatedIds);
+         ReloadAllElementDictionary();
+      }
 
-            _allElements = acumulatedIds; //model could have been changed
-        }
-    }
+      private void ReloadAllElementDictionary()
+      {
+         //Don't reuse _allElements! 
+         //if filled, it will throw a DuplicatedIdException for each call
+         var acumulatedIds = new Dictionary<string, Element>();
+         ValidateId(acumulatedIds);
+
+         _allElements = acumulatedIds; //model could have been changed
+      }
+
+      private protected override void OnFireChanging()
+      {
+         ReloadAllElementDictionary();
+      }
+   }
 }
